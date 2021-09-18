b5759625c449726cfb472f2bf0fdefc4b9b6aa66
chrisgithub <chris.a.d.kemp@gmail.com>
2021-09-18 17:35:05 +0100

    validation string length on comment+post model

--- micros/comments/models/comment_model.go
+++ micros/comments/models/comment_model.go
@@ -4,6 +4,14 @@ import (
 	uuid "github.com/gofrs/uuid"
 )
 
+// func (s *CommentModel) SetField(value string) error {
+// 	if len(value) > 1500 {
+// 		return error.New("Maximun length of Comment Text exceeded")
+// 	}
+// 	s.Text =< value
+// 	return nil
+// }
+
 type CommentModel struct {
 	ObjectId         uuid.UUID `json:"objectId"`
 	Score            int64     `json:"score"`
@@ -11,7 +19,7 @@ type CommentModel struct {
 	OwnerDisplayName string    `json:"ownerDisplayName"`
 	OwnerAvatar      string    `json:"ownerAvatar"`
 	PostId           uuid.UUID `json:"postId"`
-	Text             string    `json:"text"`
+	Text             string    `json:"text, validate:"max=1500"`
 	Deleted          bool      `json:"deleted"`
 	DeletedDate      int64     `json:"deletedDate"`
 	CreatedDate      int64     `json:"created_date"`
