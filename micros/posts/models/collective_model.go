614d0011b197d40b846bfa5254be49e678179d2e
chrisgithub <chris.a.d.kemp@gmail.com>
2021-09-22 16:42:47 +0100

    forgot to save collective_model

--- micros/posts/models/collective_model.go
+++ micros/posts/models/collective_model.go
@@ -4,7 +4,7 @@ type CollectivesModel struct {
 	CollectiveIds string `json:"collectiveIds" bson:"collectiveIds"`
 	Name          string `json:"Name" bson:"Name", validate:"max=50"`
 	Avatar        string `json:"avatar" bson:"avatar", validate:"max=500"`
-	Banner        string `json:"banner" bson:"banner", validate:"max=500"`
+	Banner        string `json:"banner", bson:"banner", validate:"max=500"`
 	CreatedDate   int64  `json:"created_date" bson:"created_date"`
 	LastUpdated   int64  `json:"last_updated" bson:"last_updated"`
 	VoteCount     int64  `json:"voteCount" bson:"voteCount"`
